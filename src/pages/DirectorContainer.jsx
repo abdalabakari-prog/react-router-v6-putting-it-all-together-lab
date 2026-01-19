dd92be1d1a30bbcddacfcf46f1b1516324f07d3a
abdalabakari-prog <abdala.bakari@student.moringaschool.com>
2026-01-19 13:37:40 +0300

    Update DirectorContainer.jsx

--- src/pages/DirectorContainer.jsx
+++ src/pages/DirectorContainer.jsx
@@ -1,28 +1,40 @@
-import { useEffect, useState } from 'react';
-import NavBar from '../components/NavBar';
+
+import { useEffect, useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import NavBar from '../components/NavBar'
 
 const DirectorContainer = () => {
-    const [directors, setDirectors] = useState([])
+  const [directors, setDirectors] = useState([])
 
-    useEffect(() => {
-        fetch("http://localhost:4000/directors")
-        .then(r => {
-            if (!r.ok) { throw new Error("failed to fetch directors") }
-            return r.json()
-        })
-        .then(setDirectors)
-        .catch(console.log)
-    }, [])
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+      .then(r => {
+        if (!r.ok) { throw new Error("failed to fetch directors") }
+        return r.json()
+      })
+      .then(setDirectors)
+      .catch(console.log)
+  }, [])
 
-    return (
-        <>
-            <NavBar />
-            <main>
-                <h1>Welcome to the Director's Directory!</h1>
-                {/* all director components should render here depending on route */}
-            </main>
-        </>
-    );
+  const addDirector = (newDirector) => {
+    setDirectors([...directors, newDirector])
+  }
+
+  const updateDirector = (updatedDirector) => {
+    setDirectors(directors.map(d => 
+      d.id === updatedDirector.id ? updatedDirector : d
+    ))
+  }
+
+  return (
+    <>
+      <NavBar />
+      <main>
+        <h1>Welcome to the Director's Directory!</h1>
+        <Outlet context={{ directors, addDirector, updateDirector }} />
+      </main>
+    </>
+  )
 }
 
-export default DirectorContainer;
+export default DirectorContainer

8b7f5129e63acec14aff3ff57c3cf7ec8ad09497
abdalabakari-prog <abdala.bakari@student.moringaschool.com>
2026-02-03 12:07:30 +0300

    Update MovieCard.jsx

--- src/pages/MovieCard.jsx
+++ src/pages/MovieCard.jsx
@@ -11,36 +11,11 @@ function MovieCard() {
   }
 
   return (
-    <>
+    <div>
       <h3>{movie.title}</h3>
       <p>{movie.year}</p>
-    </>
+    </div>
   );
 }
 
 export default MovieCard;
-
-
-// import React from 'react';
-// import { useParams, useOutletContext } from 'react-router-dom';
-
-// function MovieCard() {
-//   const { movieId } = useParams();
-//   const { director } = useOutletContext();
-
-//   const movie = director.movies[parseInt(movieId)];
-
-//   if (!movie) {
-//     return <div><h3>Movie not found</h3></div>;
-//   }
-
-//   return (
-//     <div>
-//       <h3>{movie.title}</h3>
-//       <p>Year: {movie.year}</p>
-//       <p>Director: {director.name}</p>
-//     </div>
-//   );
-// }
-
-// export default MovieCard;

713183660e27917fdbeb44948d80ca4ed153fc25
abdalabakari-prog <abdala.bakari@student.moringaschool.com>
2026-02-03 12:00:41 +0300

    Update MovieCard.jsx

--- src/pages/MovieCard.jsx
+++ src/pages/MovieCard.jsx
@@ -1,23 +1,46 @@
-import React from 'react';
-import { useParams, useOutletContext } from 'react-router-dom';
+import { useParams, useOutletContext } from "react-router-dom";
 
 function MovieCard() {
   const { movieId } = useParams();
   const { director } = useOutletContext();
 
-  const movie = director.movies[parseInt(movieId)];
+  const movie = director.movies[movieId];
 
   if (!movie) {
-    return <div><h3>Movie not found</h3></div>;
+    return <h3>Movie not found</h3>;
   }
 
   return (
-    <div>
+    <>
       <h3>{movie.title}</h3>
-      <p>Year: {movie.year}</p>
-      <p>Director: {director.name}</p>
-    </div>
+      <p>{movie.year}</p>
+    </>
   );
 }
 
 export default MovieCard;
+
+
+// import React from 'react';
+// import { useParams, useOutletContext } from 'react-router-dom';
+
+// function MovieCard() {
+//   const { movieId } = useParams();
+//   const { director } = useOutletContext();
+
+//   const movie = director.movies[parseInt(movieId)];
+
+//   if (!movie) {
+//     return <div><h3>Movie not found</h3></div>;
+//   }
+
+//   return (
+//     <div>
+//       <h3>{movie.title}</h3>
+//       <p>Year: {movie.year}</p>
+//       <p>Director: {director.name}</p>
+//     </div>
+//   );
+// }
+
+// export default MovieCard;

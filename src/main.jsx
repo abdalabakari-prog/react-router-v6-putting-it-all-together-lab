5388bb1d288b81e3b2fb0f24be0246c208dff749
abdalabakari-prog <abdala.bakari@student.moringaschool.com>
2026-02-03 11:12:33 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
 
-import "./index.css"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import App from "./App"
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);

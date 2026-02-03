8c7cd1dbc3ef797b09a43ec0fb5850ce91c43f7d
abdalabakari-prog <abdala.bakari@student.moringaschool.com>
2026-02-03 11:17:21 +0300

    Update DirectorCard.jsx

--- src/pages/DirectorCard.jsx
+++ src/pages/DirectorCard.jsx
@@ -1,32 +1,49 @@
-
-import { useParams, useOutletContext, Outlet, Link } from "react-router-dom"
+import React from 'react';
+import { useParams, useOutletContext, Outlet, Link } from 'react-router-dom';
 
 function DirectorCard() {
-  const { id } = useParams()
-  const { directors, updateDirector } = useOutletContext()
+  const { id } = useParams();
+  const { directors, updateDirector } = useOutletContext();
   
-  const director = directors.find(d => d.id === parseInt(id))
+  const director = directors.find(d => d.id === parseInt(id));
 
   if (!director) {
-    return <h2>Director not found.</h2>
+    return <div><h2>Director not found</h2></div>;
   }
+
+  const addMovie = (newMovie) => {
+    fetch(`http://localhost:3000/directors/${director.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        movies: [...director.movies, newMovie]
+      }),
+    })
+      .then(res => res.json())
+      .then(updatedDirector => {
+        updateDirector(updatedDirector);
+      });
+  };
 
   return (
     <div>
       <h2>{director.name}</h2>
-      <p>{director.bio}</p>
       <h3>Movies:</h3>
+      <Link to={`/directors/${director.id}/movies/new`}>Add New Movie</Link>
       <ul>
-        {director.movies.map((movie) => (
-          <li key={movie.id}>
-            <Link to={`/directors/${id}/movies/${movie.id}`}>{movie.title}</Link>
+        {director.movies.map((movie, index) => (
+          <li key={index}>
+            <Link to={`/directors/${director.id}/movies/${index}`}>
+              {movie.title}
+            </Link>
           </li>
         ))}
       </ul>
-      <Link to={`/directors/${id}/movies/new`}>Add New Movie</Link>
-      <Outlet context={{ director, updateDirector }} />
+      <Outlet context={{ director, addMovie }} />
     </div>
-  )
+  );
 }
 
-export default DirectorCard
+export default DirectorCard;
